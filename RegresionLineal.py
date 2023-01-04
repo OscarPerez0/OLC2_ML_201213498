77f718303c91700fd9e010726876090870c72cf4
Oscar Perez <oscar.6293@gmail.com>
2023-01-03 18:32:56 -0600

    Create RegresionLineal.py
    Added Regresion Lineal, working

---
+++ RegresionLineal.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+
+
+def RL(_info):
+    print("Ingreso a regresion Lineal") #print server
+    st.title('Regresion lineal') #titulo pagina
+    st.subheader('Informacion obtenida')
+    st.write(_info)
+    st.subheader('Parametros para evaluacion')
+    col1,col2 = st.columns(2)
+    with col1:
+        paramx = st.text_input('Ingrese parametro X','NO') #el NO varia segun lo ingresado en el input
+    
+    with col2:
+        paramy = st.text_input('Ingrese parametro Y','A') #el A varia segun lo ingresado en el input
+   
+
+    #inicio del Algoritmo de  regresion lineal
+    x = np.asarray(_info[paramx]).reshape(-1,1)
+    y = _info[paramy]
+    
+    regr = linear_model.LinearRegression()
+    regr.fit(x,y)
+
+    y_pred = regr.predict(x)
+    regresion = regr.coef_
+
+    #prueba de resultado
+  
+
+    st.subheader('Resultados')
+    
+    fig, ax = plt.subplots()
+    ax.scatter(x,y, color='black')
+    ax.plot(x,y_pred,color='red')
+    plt.title('Regresio lineal\nCoeficiente de regresion: '+str(regresion))#,'  con un error cuadratico: ',mean_squared_error(y,y_pred))
+    plt.xlabel(paramx)
+    plt.ylabel(paramy)
+    plt.grid()
+    #st.pyplot(fig)
+    texto = str(round(regr.coef_[0],2))+"X+"+str(round(regr.intercept_,2))
+    print("Ecuacion: ",texto)
+    d = {'Coeficiente de regresion': [regresion], 'Error cuadratico':[mean_squared_error(y,y_pred)], 'Coeficinte de determinacion':[r2_score(y,y_pred)],'Ecuacion lineal Ax + B':texto}
+    dresult = pd.DataFrame(data=d)
+    st.dataframe(dresult)
+
+    with st.expander("Grafica regresion lineal"):
+        st.pyplot(fig)
+    
+    with st.expander("Grafica de Puntos"):
+        fig2,ax2 = plt.subplots()
+        ax2.scatter(x,y, color='red')
+        st.pyplot(fig2)
+
+#ingresando valor para aproximar
+    c1,c2,c3 = st.columns(3)
+    with c2:
+       
+        calcular = st.text_input('Ingrese valor para aproximar con este modelo: ','0')
+        variable = regr.predict([[int(calcular)]])
+        st.text(variable)
